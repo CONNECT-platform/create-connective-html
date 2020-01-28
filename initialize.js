33fc5a006d4589c2e20e37a15bc173c4e7de9767
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-01-28 21:11:15 +0100

    bump version, fix log styling

--- initialize.js
+++ initialize.js
@@ -27,20 +27,26 @@ if (process.argv.length > 2) {
   shell.cd(`./${PROJECT_NAME}`);
 }
 
+shell.echo('');
+
 shell.echo(chalk`{bold.hex('4A90E2') #}`);
 shell.echo(chalk`{bold.hex('4A90E2') #} Creating a new {bold.hex('4A90E2') CONNECTIVE HTML} project ...`);
 shell.echo(chalk`{bold.hex('4A90E2') #} Project Name: {bold.underline.cyan ${PROJECT_NAME}}`);
 
+shell.echo(chalk`{bold.hex('4A90E2') #}`);
 shell.echo(chalk`{bold.hex('4A90E2') #} Fetching files from starter project ...`);
 shell.echo(chalk`{bold.hex('4A90E2') #}`);
 
+shell.echo('');
 shell.exec('git clone https://github.com/loreanvictor/connective-html-sample.git');
 shell.cp('-rf', './connective-html-sample/src', './');
 shell.cp('-f', './connective-html-sample/package.json', './');
 shell.cp('-f', './connective-html-sample/tsconfig.json', './');
 shell.cp('-f', './connective-html-sample/webpack.conf.ts', './');
 shell.rm('-rf', './connective-html-sample');
+shell.echo('');
 
+shell.echo(chalk`{bold.hex('4A90E2') #}`);
 shell.echo(chalk`{bold.hex('4A90E2') #} Updating {yellowBright package.json} ...`);
 shell.echo(chalk`{bold.hex('4A90E2') #}`);
 
@@ -54,15 +60,18 @@ delete pjson["bugs"];
 fs.writeFileSync('./package.json', JSON.stringify(pjson, null, 2), 'utf8');
 
 
+shell.echo(chalk`{bold.hex('4A90E2') #}`);
 shell.echo(chalk`{bold.hex('4A90E2') #} Installing dependencies ...`);
 shell.echo(chalk`{bold.hex('4A90E2') #}`);
 
 shell.exec('npm i');
 
-shell.echo(chalk`{bold.hex('76FF03') #}`);
-shell.echo(chalk`{bold.hex('76FF03') # PROJECT SUCCESSFULLY CREATED!}`);
-shell.echo(chalk`{bold.hex('76FF03') #}`);
-shell.echo(chalk`{bold.hex('76FF03') # -->} run {cyan npm start}`);
-shell.echo(chalk`{bold.hex('76FF03') # -->} open a browser on {underline.magenta.bold localhost:3000}`);
-shell.echo(chalk`{bold.hex('76FF03') # -->} check contents of {bold.gray src/} folder`);
-shell.echo(chalk`{bold.hex('76FF03') #}`);
+shell.echo('');
+shell.echo(chalk`{hex('76FF03') #}`);
+shell.echo(chalk`{hex('76FF03') # PROJECT SUCCESSFULLY CREATED!}`);
+shell.echo(chalk`{hex('76FF03') #}`);
+shell.echo(chalk`{hex('76FF03') # -->} run {cyan npm start}`);
+shell.echo(chalk`{hex('76FF03') # -->} open a browser on {underline.magenta.bold localhost:3000}`);
+shell.echo(chalk`{hex('76FF03') # -->} check contents of {bold.gray src/} folder`);
+shell.echo(chalk`{hex('76FF03') #}`);
+shell.echo('');
